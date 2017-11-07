3057bffc643852dd23c2af61c98a9ce527b9b20e
alkononovich <al.kononovich@gmail.com>
2017-11-07 12:06:18 +0300

    fix DI cache

--- lesson7/book-shop/DI/src/com/senla/training/kononovich/dependencyinjection/DependencyInjection.java
+++ lesson7/book-shop/DI/src/com/senla/training/kononovich/dependencyinjection/DependencyInjection.java
@@ -1,19 +1,32 @@
 package com.senla.training.kononovich.dependencyinjection;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
 import com.senla.training.kononovich.dependencyinjection.propertyworker.PropertyWorker;
 
 public class DependencyInjection {
+	private static final Logger logger = Logger.getLogger(DependencyInjection.class);
 	private static final String DEPENDENCY_INJECTION = "DependencyInjection.txt";
-	private static PropertyWorker propertyWorker = new PropertyWorker();
+
+	private static Map<String, Object> objects = new HashMap<>();
 
 	public static Object getClassInstance(Class<?> work) {
 		Object obj = null;
-		String implClassName = propertyWorker.getProps(DEPENDENCY_INJECTION, work.getName());
-		try {
-			Class<?> implClass = Class.forName(implClassName);
-			obj = implClass.newInstance();
-		} catch (Exception e) {
-			System.out.println(e);
+		if (objects.containsKey(work.getName())) {
+			obj = objects.get(work.getName());
+		} else {
+			PropertyWorker propertyWorker = PropertyWorker.getInstance();
+			String implClassName = propertyWorker.getProps(DEPENDENCY_INJECTION, work.getName());
+			try {
+				Class<?> implClass = Class.forName(implClassName);
+				obj = implClass.newInstance();
+				objects.put(work.getName(), obj);
+			} catch (Exception e) {
+				logger.error(e.getMessage(), e);	
+			}
 		}
 		return obj;
 	}
